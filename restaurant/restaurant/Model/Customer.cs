571ce7969b1dc866c56ae423e8568ae136e3bc67
Sumit2399 <sumit2399@gmail.com>
2023-05-02 23:27:42 +0530

    commit

---
+++ restaurant/restaurant/Model/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace restaurant.Model
+{
+	public class Customer
+	{
+        [Key]
+        public int CustomerID { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string? CustomerName { get; set; }
+    }
+}
+
---
+++ restaurant/restaurant/Program.cs
@@ -0,0 +1,45 @@
+﻿using restaurant.Model;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddDbContext<RestaurantDbContext>();
+
+string _conn = builder.Configuration.GetConnectionString("con");
+builder.Services.AddDbContextPool<RestaurantDbContext>(options=>
+options.UseMySql(_conn,ServerVersion.AutoDetect(_conn)));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000"
+                                              ).AllowAnyHeader()
+                                                  .AllowAnyMethod();
+                      });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
